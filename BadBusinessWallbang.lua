e2c2e0f1f1478b629ed74588b32f6807017202b2
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2021-10-29 03:25:45 -0400

    Delete BadBusinessWallbang.lua

--- BadBusinessWallbang.lua
+++
@@ -1,9 +0,0 @@
-local Raycast = require(game:GetService("ReplicatedStorage").TS).Raycast
-
-if shared.WallbangEnabled == true then
-    debug.setupvalue(Raycast.CastGeometryAndEnemies, 1, nil)
-    debug.setupvalue(Raycast.CastGeometryAndEnemies, 2, nil)
-else 
-    debug.setupvalue(Raycast.CastGeometryAndEnemies, 1, workspace.Geometry)
-    debug.setupvalue(Raycast.CastGeometryAndEnemies, 2, workspace.Terrain)
-end
