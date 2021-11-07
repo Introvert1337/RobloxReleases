89ca53e235b87ec14df4762f539a7580fa57ca34
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2021-11-06 20:26:56 -0400

    unpatched (that took way too fucking long)

--- Scripts/Type_Racer_Auto_Player.lua
+++ Scripts/Type_Racer_Auto_Player.lua
@@ -10,44 +10,53 @@ local getconstant = getconstant or debug.getconstant
 local getupvalue = getupvalue or debug.getupvalue 
 local getconnections = getconnections or get_signal_cons
 
+local wait = wait
+local twait = task.wait
+local v3new = Vector3.new
+
 --// Init Variables
 
 local Player = game:GetService("Players").LocalPlayer
 local OnTyped = game:GetService("ReplicatedStorage").Events.OnTyped
 local HttpService = game:GetService("HttpService")
-local Stepped = game:GetService("RunService").Stepped
 
 local CamPart = workspace:WaitForChild("CamPart")
 
 local RaceTextBox = Player.PlayerGui:WaitForChild("ScreenGui"):WaitForChild("Main"):WaitForChild("RaceScreen"):WaitForChild("TextBox")
 local RaceTextChanged = getconnections(RaceTextBox:GetPropertyChangedSignal("Text"))[1].Function 
 
-local WaitTime = shared.WaitTime and shared.WaitTime / 1000 or 0
+local WaitTime = shared.WaitTime and math.clamp(shared.WaitTime / 1000, 0.04, math.huge) or 0.04
+
+--// Wait Until Round Has Begun 
+
+while not getupvalue(RaceTextChanged, 5) do 
+    wait() 
+end
 
 --// Typing Functions & Utilities
 
 local PlayerEnvironments = getupvalue(RaceTextChanged, 3)
 local Keys = getupvalue(RaceTextChanged, 4)
-local Encrypt = getupvalue(RaceTextChanged, 8)
 local Virtual = getupvalue(RaceTextChanged, 7)
+local Encrypt = getupvalue(RaceTextChanged, 8)
 local Settings = getupvalue(RaceTextChanged, 12)
 
 local IndexKey = getconstant(RaceTextChanged, 8)
 
 --// Loop Through Keys and Type Them
 
-for Index, Value in next, Keys do 
-    Virtual(Player, Value:lower())
+for Index = 1, #Keys do
+    Virtual(Player)
     
     OnTyped:FireServer(Encrypt(HttpService:JSONEncode({
         [IndexKey] = Index
     }), 10))
 
-    CamPart.Position = PlayerEnvironments[Player].letterParts[Index].Position + Vector3.new(Settings.camDistance, Settings.camHeight, 0)
+    CamPart.Position = PlayerEnvironments[Player].letterParts[Index].Position + v3new(Settings.camDistance, Settings.camHeight, 0)
     
-    if WaitTime == 0 then 
-        Stepped:Wait()
-    else 
+    if WaitTime ~= 0 then 
         wait(WaitTime)
+    else 
+        twait()
     end
 end
