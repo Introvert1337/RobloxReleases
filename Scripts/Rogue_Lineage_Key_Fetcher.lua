1d00337572c37e0488e626e6e4d5e189207b98b8
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2021-11-13 09:39:56 -0500

    major optimizations and it now doesn't error if u aren't spawned (it will wait until ur spawned in)

--- Scripts/Rogue_Lineage_Key_Fetcher.lua
+++ Scripts/Rogue_Lineage_Key_Fetcher.lua
@@ -6,7 +6,7 @@ if not game:IsLoaded() then
     game.Loaded:Wait();
 end;
 
---// localizations 
+--// variables/localizations
 
 local getupvalue = getupvalue or debug.getupvalue;
 local getupvalues = getupvalues or debug.getupvalues;
@@ -26,13 +26,17 @@ local tonumber = tonumber;
 local assert = assert;
 
 local run_service = game:GetService("RunService");
+local player = game:GetService("Players").LocalPlayer;
+
+local dependencies = {
+    psu_struct = {
+        next = "sBgaL",
+        rB = -50014
+    },
+    script_hash = "1e619fe2ee00718f0b70188beb78d3bf96f1d86f12141482ddeddf2f1216853df4846eb43b10dcd04bd8b594b3122ff0"
+};
 
 --// method patcher (credit to sor for the patcher) 
-
-local psu_struct = {
-    next = "sBgaL",
-    rB = -50014
-};
 
 local function patch_method(upvalues, method)
     local instructions, stack;
@@ -42,7 +46,7 @@ local function patch_method(upvalues, method)
             local entry = upvalue[0];
 
             if entry then
-                if type(entry) == "table" and entry[psu_struct.next] then
+                if type(entry) == "table" and entry[dependencies.psu_struct.next] then
                     instructions = upvalue;
                 end;
             else
@@ -63,18 +67,18 @@ local function patch_method(upvalues, method)
         while true do 
             local instr = instructions[cur_instr];
 
-            if instr and type(instr[psu_struct.rB]) == "table" then 
+            if instr and type(instr[dependencies.psu_struct.rB]) == "table" then 
                 local success = true;
 
                 for index = 1, 5 do 
-                    if type(instructions[cur_instr + index][psu_struct.rB]) ~= "table" then 
+                    if type(instructions[cur_instr + index][dependencies.psu_struct.rB]) ~= "table" then 
                         success = false;
                     end;
                 end;
 
                 if success then
                     local to_patch = instr;
-                    local go_to = instr[psu_struct.rB];
+                    local go_to = instr[dependencies.psu_struct.rB];
 
                     for index, value in next, go_to do 
                         to_patch[index] = value;
@@ -94,35 +98,50 @@ end;
 --// check if keyhandler updated
 
 local key_handler = game:GetService("ReplicatedStorage"):WaitForChild("Assets"):WaitForChild("Modules"):WaitForChild("KeyHandler");
-assert(getscripthash(key_handler) == "1e619fe2ee00718f0b70188beb78d3bf96f1d86f12141482ddeddf2f1216853df4846eb43b10dcd04bd8b594b3122ff0", "keyhandler script updated!");
+assert(getscripthash(key_handler) == dependencies.script_hash, "keyhandler script updated!");
+
+--// wait until character spawned
+
+local character = player.Character;
+
+if not character then 
+    character = player.CharacterAdded:Wait();
+end
+
+character:WaitForChild("CharacterHandler"):WaitForChild("Input");
 
 --// gc loop to find the dodge remote fpe key
 
+local start_time = tick();
 local dodge_fpe_key;
 
-for index, value in next, getgc() do 
-    if islclosure(value) and getinfo(value).source:find("Input") then 
-        local constants = getconstants(value);
-        
-        if t_find(constants, "SpeedBoost") and t_find(constants, "HasHammer") then 
-            local dodge_function = getproto(value, 1);
-
-            setupvalue(dodge_function, 1, function() end);
-            setupvalue(dodge_function, 2,  function(key)
-                dodge_fpe_key = tonumber(("%0.50f"):format(key)); -- terrible method
-            end);
-            setupvalue(dodge_function, 3, tonumber);
-            setupvalue(dodge_function, 4, tostring);
-            setupvalue(dodge_function, 5, function() end);
+repeat -- repeating a gc loop is usually bad it should be fine in this case considering it will probably only ever repeat once
+    for index, value in next, getgc() do 
+        if islclosure(value) and getinfo(value).source:find("Input") then 
+            local constants = getconstants(value);
             
-            dodge_function();
+            if t_find(constants, "SpeedBoost") and t_find(constants, "HasHammer") then 
+                local dodge_function = getproto(value, 1);
 
-            break
+                setupvalue(dodge_function, 1, function() end);
+                setupvalue(dodge_function, 2,  function(key)
+                    dodge_fpe_key = tonumber(("%0.50f"):format(key)); -- terrible method
+                end);
+                setupvalue(dodge_function, 3, tonumber);
+                setupvalue(dodge_function, 4, tostring);
+                setupvalue(dodge_function, 5, function() end);
+                
+                dodge_function();
+
+                break
+            end;
         end;
     end;
-end;
 
-assert(dodge_fpe_key, "unable to find dodge fpe key!");
+    if not dodge_fpe_key then
+        wait(1);
+    end;
+until dodge_fpe_key;
 
 --// patch module and getkey function
 
@@ -143,32 +162,34 @@ wait_hook = hookfunction(wait, function(t)
     return wait_hook(t);
 end);
 
---// function to grab a remote 
+--// remote fetcher
 
-local area_client_connection_function;
+local area_client_connection_function, area_client_remote_function;
+
+repeat 
+    for index, connection in next, getconnections(run_service.RenderStepped) do 
+        local connection_function = connection.Function;
+        
+        if type(connection_function) == "function" and getinfo(connection_function).source:find("AreaClient") then 
+            area_client_connection_function = connection_function;
+            area_client_remote_function = getupvalue(area_client_connection_function, 5);
+        end;
+    end;
+
+    t_wait();
+until area_client_connection_function and area_client_remote_function;
 
 getgenv().get_remote = function(remote_name)
-    if not area_client_connection_function then 
-        for index, connection in next, getconnections(run_service.RenderStepped) do 
-            local connection_function = connection.Function;
-            
-            if type(connection_function) == "function" and getinfo(connection_function).source:find("AreaClient") then 
-                area_client_connection_function = connection_function;
-            end;
-        end;
-    end; 
-
     local remote;
     
     local old_upvalues = getupvalues(area_client_connection_function);
-    local remote_function = old_upvalues[5];
-    local old_remote_upvalues = getupvalues(area_client_connection_function);
+    local old_remote_upvalues = getupvalues(area_client_remote_function);
     
-    setupvalue(remote_function, 5, remote_name == "Dodge" and dodge_fpe_key or remote_name);
-    setupvalue(remote_function, 6, function() end);
-    setupvalue(remote_function, 3, function(fired_remote)
+    setupvalue(area_client_remote_function, 5, remote_name == "Dodge" and dodge_fpe_key or remote_name);
+    setupvalue(area_client_remote_function, 6, function() end);
+    setupvalue(area_client_remote_function, 3, function(fired_remote)
         remote = fired_remote;
-        setupvalue(remote_function, 1, false);
+        setupvalue(area_client_remote_function, 1, false);
         return;
     end);
     
@@ -184,9 +205,11 @@ getgenv().get_remote = function(remote_name)
     setupvalue(area_client_connection_function, 6, old_upvalues[6]);
     setupvalue(area_client_connection_function, 7, old_upvalues[7]);
 
-    setupvalue(remote_function, 3, old_remote_upvalues[3]);
-    setupvalue(remote_function, 5, old_remote_upvalues[5]);
-    setupvalue(remote_function, 6, old_remote_upvalues[6]);
+    setupvalue(area_client_remote_function, 3, old_remote_upvalues[3]);
+    setupvalue(area_client_remote_function, 5, old_remote_upvalues[5]);
+    setupvalue(area_client_remote_function, 6, old_remote_upvalues[6]);
 
     return remote;
 end;
+
+warn(("KeyHandler Bypass Loaded, Took %s"):format(tick() - start_time));
