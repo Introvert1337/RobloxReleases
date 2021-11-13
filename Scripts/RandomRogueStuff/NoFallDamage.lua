865e0be4e77f28b36d7250c077b50135b331fc6f
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2021-11-13 11:46:51 -0500

    Update NoFallDamage.lua

--- Scripts/RandomRogueStuff/NoFallDamage.lua
+++ Scripts/RandomRogueStuff/NoFallDamage.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/master/Scripts/Rogue_Lineage_Key_Fetcher.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/master/Scripts/Rogue_Lineage_Key_Fetcher.lua"))();
 
 local apply_fall_damage_remote = get_remote("ApplyFallDamage");
 
