a0e5279dea76fb226a266b45772af919a573ca7c
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2021-11-14 15:00:54 -0500

    added redeemcode and error handling

--- Scripts/Jailbreak_Key_Fetcher.lua
+++ Scripts/Jailbreak_Key_Fetcher.lua
@@ -32,27 +32,28 @@ local v3_new = Vector3.new;
 --// Init Variables 
 
 local dependencies = {
-    network = getupvalue(require(replicated_storage.Module.AlexChassis).SetEvent, 1);
-    start_time = tick();
-    marked_functions = {};
-    network_keys = {};
-    keys_list = {"Punch", "Hijack", "Kick", "CarKick", "FallDamage", "PopTire", "SwitchTeam", "BroadcastInputBegan", "BroadcastInputEnded", "Arrest", "Eject", "EnterCar", "ExitCar", "SwitchTeam", "PlaySound", "SpawnCar"};
+    network = getupvalue(require(replicated_storage.Module.AlexChassis).SetEvent, 1),
+    start_time = tick(),
+    marked_functions = {},
+    network_keys = {},
+    keys_list = {"Punch", "Hijack", "Kick", "CarKick", "FallDamage", "PopTire", "SwitchTeam", "BroadcastInputBegan", "BroadcastInputEnded", "Arrest", "Eject", "EnterCar", "ExitCar", "SwitchTeam", "PlaySound", "SpawnCar", "RedeemCode"};
     modules = {
-        default_actions = require(replicated_storage.Game.DefaultActions);
-        military_turret_system = require(replicated_storage.Game.MilitaryTurret.MilitaryTurretSystem);
-        team_choose_ui = require(replicated_storage.Game.TeamChooseUI);
-        item_system = require(replicated_storage.Game.ItemSystem.ItemSystem);
-        taser = require(replicated_storage.Game.Item.Taser);
-        gun = require(replicated_storage.Game.Item.Gun);
-        falling = require(replicated_storage.Game.Falling);
-        garage_ui = require(replicated_storage.Game.Garage.GarageUI);
+        default_actions = require(replicated_storage.Game.DefaultActions),
+        military_turret_system = require(replicated_storage.Game.MilitaryTurret.MilitaryTurretSystem),
+        team_choose_ui = require(replicated_storage.Game.TeamChooseUI),
+        item_system = require(replicated_storage.Game.ItemSystem.ItemSystem),
+        taser = require(replicated_storage.Game.Item.Taser),
+        gun = require(replicated_storage.Game.Item.Gun),
+        falling = require(replicated_storage.Game.Falling),
+        garage_ui = require(replicated_storage.Game.Garage.GarageUI),
+        codes = require(replicated_storage.Game.Codes)
     };
 };
 
 local functions = {
     mark_function = function(func, name)
         dependencies.marked_functions[func] = {name = name};
-    end;
+    end,
 
     hook_fire_server = function(func, index, name)
         setupvalue(func, index, {
@@ -60,7 +61,7 @@ local functions = {
                 dependencies.network_keys[name] = key;
             end;
         });
-    end;
+    end
 };
 
 --// Network Hook (credit to senser for the "marking" method)
@@ -103,7 +104,12 @@ do
         for index, constant in next, constants do 
             if constant == "Play" and constants[index - 1] == "LoadAnimation" then 
                 setconstant(punch_function, index, "Stop");
-                punch_function();
+                pcall(punch_function);
+
+                if getupvalue(punch_function, 1) == 0 then 
+                    setupvalue(punch_function, 1, tick());
+                end;
+
                 setconstant(punch_function, index, "Play");
 
                 break;
@@ -127,7 +133,7 @@ do
                 pcall = function() return false end;
             });
         
-            kick_function();
+            pcall(kick_function);
         
             setupvalue(kick_function, 2, false);
             setfenv(kick_function, old_environment);
@@ -136,7 +142,7 @@ do
         do -- carkick
             functions.hook_fire_server(car_kick_function, 1, "CarKick");
 
-            car_kick_function();
+            pcall(car_kick_function);
         end;
     end;
 
@@ -145,7 +151,7 @@ do
 
         functions.hook_fire_server(damage_function, 1, "Damage");
 
-        damage_function();
+        pcall(damage_function);
     end;
 
     do -- switchteam
@@ -153,7 +159,7 @@ do
 
         functions.hook_fire_server(switch_team_function, 2, "SwitchTeam");
 
-        switch_team_function();
+        pcall(switch_team_function);
     end;
 
     do -- broadcastinputbegan / broadcastinputended
@@ -163,12 +169,12 @@ do
 
         do -- broadcastinputbegan
             functions.hook_fire_server(input_began_function, 1, "BroadcastInputBegan");
-            input_began_function(true, {});
+            pcall(input_began_function, true, {});
         end;
 
         do -- broadcastinputended
             functions.hook_fire_server(input_ended_function, 1, "BroadcastInputEnded");
-            input_ended_function(true, {});
+            pcall(input_ended_function, true, {});
         end;
     end;
 
@@ -179,42 +185,25 @@ do
         functions.mark_function(taze_function, "Taze");
 
         setupvalue(taze_function, 1, {
-            getAttr = function() 
-                return 0; 
-            end;
-            setAttr = function() 
-                setupvalue(taze_function, 1, old_upvalues[1]); 
-            end;
+            getAttr = function() return 0; end;
+            setAttr = function() end;
         });
-        setupvalue(taze_function, 2, {
-            ObjectLocal = function() 
-                setupvalue(taze_function, 2, old_upvalues[2]); 
-            end;
-        });
-        setupvalue(taze_function, 3, {
-            GetPlayers = function() 
-                setupvalue(taze_function, 3, players); 
-                return {}; 
-            end;
-        });
-        setupvalue(taze_function, 5, {
-            RayIgnoreNonCollideWithIgnoreList = function() 
-                setupvalue(taze_function, 5, old_upvalues[5]); 
-                return true; 
-            end;
-        });
-        setupvalue(taze_function, 6, {
-            getPlayerFromDescendant = function() 
-                setupvalue(taze_function, 6, old_upvalues[6]); 
-                return {Name = ""};
-            end;
-        });
+        setupvalue(taze_function, 2, {ObjectLocal = function() end});
+        setupvalue(taze_function, 3, {GetPlayers = function() return {}; end});
+        setupvalue(taze_function, 5, {RayIgnoreNonCollideWithIgnoreList = function() return true; end});
+        setupvalue(taze_function, 6, {getPlayerFromDescendant = function() return {Name = ""}; end});
 
-        taze_function({
+        pcall(taze_function, {
             ItemData = {NextUse = 0}, CrossHair = {Flare = function() end, Spring = {Accelerate = function() end}}, 
             Config = {Sound = {tazer_buzz = 0}, ReloadTime = 0, ReloadTimeHit = 0}, IgnoreList = {}, Draw = function() end, BroadcastInputBegan = function() end, 
             UpdateMousePosition = function() end, Tip = {Position = v3_new(0, 0, 0)}, Local = true, MousePosition = v3_new(0, 0, 0)
         });
+
+        setupvalue(taze_function, 1, old_upvalues[1]); 
+        setupvalue(taze_function, 2, old_upvalues[2]); 
+        setupvalue(taze_function, 3, players); 
+        setupvalue(taze_function, 5, old_upvalues[5]); 
+        setupvalue(taze_function, 6, old_upvalues[6]); 
     end;
 
     do -- playsound 
@@ -230,12 +219,13 @@ do
                         local old_upvalue = getupvalue(play_sound_function, 2);
                         setupvalue(play_sound_function, 2, setmetatable({}, {
                             __index = function() 
-                                setupvalue(play_sound_function, 2, old_upvalue);
                                 return function() end;
                             end;
                         }));
 
-                        play_sound_function(nil, nil);
+                        pcall(play_sound_function, nil, nil);
+
+                        setupvalue(play_sound_function, 2, old_upvalue);
                     end;
                 end;
             end;
@@ -252,12 +242,12 @@ do
 
         do -- hijack
             functions.mark_function(hijack_function, "Hijack");
-            hijack_function();
+            pcall(hijack_function);
         end;
 
         do -- eject
             functions.mark_function(eject_function, "Eject");
-            eject_function();
+            pcall(eject_function);
         end;
 
         do -- entercar
@@ -266,7 +256,7 @@ do
             local old_upvalue = getupvalue(enter_car_function, 1);
             setupvalue(enter_car_function, 1, nil);
 
-            enter_car_function({});
+            pcall(enter_car_function, {});
 
             setupvalue(enter_car_function, 1, old_upvalue);
         end;
@@ -290,7 +280,7 @@ do
 
         functions.hook_fire_server(bullet_function, 7, "PopTire");
 
-        bullet_function({Color = Color3.new(0, 0, 0), IsDescendantOf = function(self, obj) return obj.Name == "ShootingRange" end}, v3_new(0, 0, 0), v3_new(0, 0, 0), 0);
+        pcall(bullet_function, {Color = Color3.new(0, 0, 0), IsDescendantOf = function(self, obj) return obj.Name == "ShootingRange" end}, v3_new(0, 0, 0), v3_new(0, 0, 0), 0);
     end;
 
     do -- pickpocket / arrest
@@ -301,12 +291,12 @@ do
 
         do -- pickpocket
             functions.mark_function(pickpocket_function, "Pickpocket");
-            pickpocket_function({});
+            pcall(pickpocket_function, {});
         end;
 
         do -- arrest
             functions.mark_function(arrest_function, "Arrest");
-            arrest_function({});
+            pcall(arrest_function, {});
         end;
     end;
 
@@ -324,7 +314,7 @@ do
         setupvalue(fall_function, 5, 25);
         setupvalue(fall_function, 6, true);
 
-        fall_function(0);
+        pcall(fall_function, 0);
 
         for index = 1, 6 do 
             if index ~= 2 then 
@@ -341,18 +331,13 @@ do
         local old_upvalues = getupvalues(exit_car_function);
 
         setupvalue(exit_car_function, 1, true);
-        setupvalue(exit_car_function, 2, {
-            OnVehicleJumpExited = {
-                Fire = function() 
-                    setupvalue(exit_car_function, 2, old_upvalues[2]);
-                end;
-            };
-        });
+        setupvalue(exit_car_function, 2, {OnVehicleJumpExited = {Fire = function() end}});
         setupvalue(exit_car_function, 6, {Heli = {}});
 
-        exit_car_function();
+        pcall(exit_car_function);
 
         setupvalue(exit_car_function, 1, old_upvalues[1]);
+        setupvalue(exit_car_function, 2, old_upvalues[2]);
         setupvalue(exit_car_function, 6, old_upvalues[6]);
     end;
 
@@ -361,7 +346,18 @@ do
 
         functions.hook_fire_server(spawn_car_function, 1, "SpawnCar");
 
-        spawn_car_function({});
+        pcall(spawn_car_function, {});
+    end;
+    
+    do -- redeemcode
+        local redeem_code_function = getproto(dependencies.modules.codes.Init, 4);
+        
+        setupvalue(redeem_code_function, 1, {CodeContainer = {Background = {CodeContainer = {Code = {Text = " "}}}}});
+        setupvalue(redeem_code_function, 3, {WindowClose = function() end})
+        
+        functions.hook_fire_server(redeem_code_function, 2, "RedeemCode");
+        
+        pcall(redeem_code_function);
     end;
 end;
 
