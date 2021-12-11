d6a8a190a4a4e3e28b5b7540680b83ed5ae84cfc
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2021-12-11 14:21:19 -0500

    rewritten and removed useless optimizations

--- Scripts/Type_Racer_Auto_Player.lua
+++ Scripts/Type_Racer_Auto_Player.lua
@@ -1,62 +1,48 @@
---// Wait Until Game is Loaded 
+--// wait until game is loaded 
 
 if not game:IsLoaded() then 
-    game.Loaded:Wait()
-end 
+    game.Loaded:Wait();
+end;
 
---// Localizations
+--// init variables
 
-local getconstant = getconstant or debug.getconstant
-local getupvalue = getupvalue or debug.getupvalue 
-local getconnections = getconnections or get_signal_cons
+local player = game:GetService("Players").LocalPlayer;
+local http_service = game:GetService("HttpService");
+local on_typed = game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("OnTyped");
 
-local wait = wait
-local twait = task.wait
-local v3new = Vector3.new
+local cam_part = workspace:WaitForChild("CamPart");
 
---// Init Variables
+local race_text_box = player.PlayerGui:WaitForChild("ScreenGui"):WaitForChild("Main"):WaitForChild("RaceScreen"):WaitForChild("TextBox");
+local race_text_changed = getconnections(race_text_box:GetPropertyChangedSignal("Text"))[1].Function;
 
-local Player = game:GetService("Players").LocalPlayer
-local OnTyped = game:GetService("ReplicatedStorage").Events.OnTyped
-local HttpService = game:GetService("HttpService")
+local wait_time = shared.wait_time and math.clamp(shared.wait_time / 1000, 0.04, math.huge) or 0.04;
 
-local CamPart = workspace:WaitForChild("CamPart")
+--// wait until round has begun 
 
-local RaceTextBox = Player.PlayerGui:WaitForChild("ScreenGui"):WaitForChild("Main"):WaitForChild("RaceScreen"):WaitForChild("TextBox")
-local RaceTextChanged = getconnections(RaceTextBox:GetPropertyChangedSignal("Text"))[1].Function 
+while not getupvalue(race_text_changed, 5) do 
+    wait();
+end;
 
-local WaitTime = shared.WaitTime and math.clamp(shared.WaitTime / 1000, 0.04, math.huge) or 0.04
+--// typing functions & utilities
 
---// Wait Until Round Has Begun 
+local player_environments = getupvalue(race_text_changed, 3);
+local keys = getupvalue(race_text_changed, 4);
+local virtual = getupvalue(race_text_changed, 7);
+local encryption = getupvalue(race_text_changed, 8);
+local game_settings = getupvalue(race_text_changed, 12);
 
-while not getupvalue(RaceTextChanged, 5) do 
-    wait() 
-end
+local index_key = getconstant(race_text_changed, 8);
 
---// Typing Functions & Utilities
+--// loop through keys and type them
 
-local PlayerEnvironments = getupvalue(RaceTextChanged, 3)
-local Keys = getupvalue(RaceTextChanged, 4)
-local Virtual = getupvalue(RaceTextChanged, 7)
-local Encrypt = getupvalue(RaceTextChanged, 8)
-local Settings = getupvalue(RaceTextChanged, 12)
-
-local IndexKey = getconstant(RaceTextChanged, 8)
-
---// Loop Through Keys and Type Them
-
-for Index = 1, #Keys do
-    Virtual(Player)
+for index = 1, #keys do
+    virtual(player);
     
-    OnTyped:FireServer(Encrypt(HttpService:JSONEncode({
-        [IndexKey] = Index
-    }), 10))
+    on_typed:FireServer(encryption(http_service:JSONEncode({
+        [index_key] = index
+    }), 10));
 
-    CamPart.Position = PlayerEnvironments[Player].letterParts[Index].Position + v3new(Settings.camDistance, Settings.camHeight, 0)
+    cam_part.Position = player_environments[player].letterParts[index].Position + Vector3.new(game_settings.camDistance, game_settings.camHeight, 0);
     
-    if WaitTime ~= 0 then 
-        wait(WaitTime)
-    else 
-        twait()
-    end
-end
+    wait(wait_time);
+end;
