a4187b17b0e475b8140c45673d6495951ccc5e9e
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2021-12-11 14:10:08 -0500

    unpatched and removed useless optimizations

--- Scripts/Galaxy_Remote_Bypass.lua
+++ Scripts/Galaxy_Remote_Bypass.lua
@@ -1,39 +1,16 @@
---// localizations 
-
-local getupvalue = getupvalue or debug.getupvalue;
-local getupvalues = getupvalues or debug.getupvalues;
-local getinfo = getinfo or debug.getinfo;
-local checkcaller = checkcaller;
-
-local type = type;
-local typeof = typeof;
-local rawget = rawget;
-
 --// init variables
 
 local replicated_storage = game:GetService("ReplicatedStorage");
 local remote_folder = replicated_storage.Remote;
 
 local remote_function = getupvalue(require(replicated_storage.Scripts.ShipPurchaseModule).ShowAvailableShips, 2);
-
+local valid_env = getupvalue(remote_function, 2);
+	
 --// bypass env checks
 
-local old_getfenv;
-old_getfenv = replaceclosure(getfenv, function(level)
-    if level == 2 then 
-        local info = getinfo(3);
-        
-        if info and info.source:find("ClientLib") then
-            local env_upvalue = getupvalues(info.func)[1];
-            
-            if type(env_upvalue) == "table" and rawget(env_upvalue, "script") then 
-                return env_upvalue;
-            end;
-        end;
-    end;
-    
-    return old_getfenv(level);
-end);
+replaceclosure(getfenv(remote_function).getfenv, newcclosure(function(level)
+	return valid_env;
+end));
 
 --// function to fire remote            
                                                 
@@ -43,6 +20,4 @@ getgenv().fire_remote = function(remote, ...)
     if remote then 
         return remote_function(remote, ...);
     end;
-    
-    return false;
 end;
