7fba64672e26e637bf1509b3f4673d95203b6d16
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2021-12-25 06:10:15 -0500

    Update HydraHubV2.lua

--- Cracks/HydraHubV2.lua
+++ Cracks/HydraHubV2.lua
@@ -1,31 +1,31 @@
-do -- hooks
-    do -- syn.request
-       replaceclosure(syn.request, newcclosure(function(payload)
-            local url = payload.Url;
-            
-            if url == "https://whitelist.hydrahub.net/api/dSgVkYp2" then 
-                return {Body = '{"1":"tmSgO26scLxO51Y","2":"7WSadFSi7idfvaQ","3":"Gf8wyS8z3pYOqkl"}'}; -- random keys
-            elseif url == "https://whitelist.hydrahub.net/api/mYq3t6w9" then
-                return {Body = "22cca489bad9cfe5a7a6cb217767e4d895d3a5bd18b9c2d7c57ed8f7dc6ef6c1"}; -- static response with spoofed time and random values
-            end;
-        end));
-    end;
+do
+    local hmac_function, uniform_rng_function = loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/master/Cracks/Dependencies/HydraHub/HashLibrary.lua"))();
+    local server_key = ("a"):rep(15);
     
-    do -- tostring (i have a os.time and math.random hook but this is shorter and avoids the dumb checks)
-        local old_tostring; 
-        old_tostring = replaceclosure(tostring, newcclosure(function(data)
-            if checkcaller() then 
-                if data == 1 or data == 2 or data == 3 then -- tostring on math.random(1, 3)
-                    return "2";
-                elseif data == os.time() then -- tostring on os.time()
-                    return "1640302130";
-                end;
-            end; 
-
-            return old_tostring(data);
-        end));
-    end;
+    replaceclosure(syn.request, newcclosure(function(payload)
+        local url = payload.Url;
+        
+        if url == "https://whitelist.hydrahub.net/api/dSgVkYp2" then 
+            return {
+                Body = game:GetService("HttpService"):JSONEncode({
+                    ["1"] = server_key, 
+                    ["2"] = server_key, 
+                    ["3"] = server_key
+                })
+            };
+        elseif url == "https://whitelist.hydrahub.net/api/mYq3t6w9" then
+            local current_time = tostring(os.time());
+            local time_data = uniform_rng_function(current_time:sub(-4, -4), current_time:sub(-3, -3));
+            local script_key = getgenv().Key or "";
+        
+            return {
+                Body = hmac_function(server_key, script_key .. tostring(time_data):sub(1, -3))
+            };
+        end;
+        
+        error("unhandled request");
+    end));
 end;
 
-getgenv().Key = "SO1WxX4QetzX878m"; -- this key is a part of the response
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RandomStuff/main/hydra.lua"))();
+getgenv().Key = ("a"):rep(16);
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/master/Cracks/Dependencies/HydraHub/RawScript.lua"))();
