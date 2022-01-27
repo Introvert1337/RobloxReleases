2ff2ef67e1209866006ab856a71585b81f9ccf3f
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2022-01-27 16:04:24 -0500

    Update Jailbreak_Key_Fetcher.lua

--- Scripts/Jailbreak_Key_Fetcher.lua
+++ Scripts/Jailbreak_Key_Fetcher.lua
@@ -1,383 +1,165 @@
---// Game Loaded
+--// Check if Already in Env 
 
-if not game:IsLoaded() then 
-    game.Loaded:Wait();
+if network_keys and network then 
+    return network_keys, network;
 end;
 
---// Localizations 
+--// Variables 
 
-local getupvalue = getupvalue or debug.getupvalue;
-local setupvalue = setupvalue or debug.setupvalue;
-local getupvalues = getupvalues or debug.getupvalues;
-local setconstant = setconstant or debug.setconstant;
-local getconstants = getconstants or debug.getconstants;
-local getinfo = getinfo or debug.getinfo;
-local getproto = getproto or debug.getproto;
-local checkcaller = checkcaller;
-local getconnections = getconnections;
-local islclosure = islclosure;
-
-local require = require;
-local tostring = tostring;
-local pcall = pcall;
-local table_find = table.find;
-local vector3_new = Vector3.new;
-
-local rconsoleprint = rconsoleprint;
-local rconsolename = rconsolename or rconsolesettitle;
-local rconsolecreate = rconsolecreate;
-local rconsolewarn = rconsolewarn or function(...) rconsoleprint(tostring(...), "yellow") end;
-local rconsolerrr = rconsolerrr or function(...) rconsoleprint(tostring(...), "red") end;
-
---// Init Variables 
+local start_time = tick();
 
 local replicated_storage = game:GetService("ReplicatedStorage");
-local players = game:GetService("Players");
 local collection_service = game:GetService("CollectionService");
 
-local dependencies = {
-    network = getupvalue(require(replicated_storage.Module.AlexChassis).SetEvent, 1),
-    start_time = tick(),
-    marked_functions = {},
-    network_keys = {},
-    keys_list = {"Punch", "Hijack", "Kick", "CarKick", "FallDamage", "SwitchTeam", "BroadcastInputBegan", "BroadcastInputEnded", "Arrest", "Eject", "EnterCar", "ExitCar", "SwitchTeam", "PlaySound", "SpawnCar", "RedeemCode"};
-    modules = {
-        default_actions = require(replicated_storage.Game.DefaultActions),
-        military_turret_system = require(replicated_storage.Game.MilitaryTurret.MilitaryTurretSystem),
-        team_choose_ui = require(replicated_storage.Game.TeamChooseUI),
-        item_system = require(replicated_storage.Game.ItemSystem.ItemSystem),
-        taser = require(replicated_storage.Game.Item.Taser),
-        gun = require(replicated_storage.Game.Item.Gun),
-        falling = require(replicated_storage.Game.Falling),
-        garage_ui = require(replicated_storage.Game.Garage.GarageUI),
-        codes = require(replicated_storage.Game.Codes)
-    };
-};
+local network = getupvalue(require(replicated_storage.Module.AlexChassis).SetEvent, 1);
+local keys_list = getupvalue(getupvalue(network.FireServer, 1), 3);
 
-local functions = {
-    mark_function = function(func, name)
-        dependencies.marked_functions[func] = {name = name};
-    end,
+local game_folder = replicated_storage.Game;
 
-    hook_fire_server = function(func, index, name)
-        setupvalue(func, index, {
-            FireServer = function(self, key)
-                dependencies.network_keys[name] = key;
-            end;
-        });
-    end
-};
+local team_choose_ui = require(game_folder.TeamChooseUI); -- module used in multiple keys
+local default_actions = require(game_folder.DefaultActions); -- module used in multiple keys
 
---// Network Hook (credit to senser for the "marking" method)
+local roblox_environment = getrenv();
+local network_keys = {};
 
-local old_fire_server = getupvalue(dependencies.network.FireServer, 1);
+--// Functions 
 
-setupvalue(dependencies.network.FireServer, 1, function(key, ...)
-    local caller_info = getinfo(2, "f");
+local function fetch_key(caller_function)
+    local constants = getconstants(caller_function);
+    
+    for index, constant in next, constants do
+        if keys_list[constant] then -- if the constants already contain the raw key
+            return constant;
+        elseif type(constant) ~= "string" or constant == "" or roblox_environment[constant] or string[constant] or table[constant] or #constant > 7 or constant:lower() ~= constant then
+            constants[index] = nil; -- remove constants that are 100% not the ones we need to make it a bit faster
+        end;
+    end;
+    
+    for key, remote in next, keys_list do 
+        local prefix_passed = false;
+        local key_length = #key;
         
-    if caller_info.func == dependencies.network.FireServer then 
-        caller_info = getinfo(3, "f");
+        for index, constant in next, constants do 
+            local constant_length = #constant;
+            
+            if not prefix_passed and key:sub(1, constant_length) == constant then -- check if the key starts with one of the constants
+                prefix_passed = constant;
+            elseif prefix_passed and constant ~= prefix_passed and key:sub(key_length - (constant_length - 1), key_length) == constant then -- check if the key ends with one of the constants
+                return key;
+            end;
+        end;
     end;
-
-    local mark_info = dependencies.marked_functions[caller_info.func];
-
-    if caller_info and mark_info and checkcaller() then 
-        dependencies.network_keys[mark_info.name] = key;
-        dependencies.marked_functions[caller_info.func] = nil;
-
-        return;
-    end;
-
-    return old_fire_server(key, ...);
-end);
+end;
 
 --// Key Fetching 
 
-do -- punch
-    local punch_function = getupvalue(dependencies.modules.default_actions.punchButton.onPressed, 1).attemptPunch;
-    
-    functions.mark_function(punch_function, "Punch");
+do -- redeemcode
+    local redeem_code_function = getproto(require(game_folder.Codes).Init, 4);
 
-    setupvalue(punch_function, 1, 0);
-
-    local constants = getconstants(punch_function);
-        
-    for index, constant in next, constants do 
-        if constant == "Play" and constants[index - 1] == "LoadAnimation" then 
-            setconstant(punch_function, index, "Stop");
-            pcall(punch_function);
-
-            if getupvalue(punch_function, 1) == 0 then 
-                setupvalue(punch_function, 1, tick());
-            end;
-
-            setconstant(punch_function, index, "Play");
-
-            break;
-        end;
-    end;
+    network_keys.RedeemCode = fetch_key(redeem_code_function);
 end;
 
-do -- kick / carkick
-    local connection = getconnections(collection_service:GetInstanceRemovedSignal("Door"))[1].Function;
-    local functions_table = getupvalue(getupvalue(getupvalue(getupvalue(connection, 2), 2).Run, 1), 1);
-
-    local car_kick_function = getproto(functions_table[3].c, 1);
-    local kick_function = functions_table[4].c;
-
-    do -- kick
-        functions.mark_function(kick_function, "Kick");
+do -- kick
+    local door_removed_signal = getconnections(collection_service:GetInstanceRemovedSignal("Door"))[1].Function;
+    local kick_function = getupvalue(getupvalue(getupvalue(getupvalue(door_removed_signal, 2), 2).Run, 1), 1)[4].c;
     
-        local old_environment = getfenv(kick_function);
+    network_keys.Kick = fetch_key(kick_function);
+end;
 
-        setfenv(kick_function, {pcall = function() return false end});
-    
-        pcall(kick_function);
-    
-        setupvalue(kick_function, 2, false);
-        setfenv(kick_function, old_environment);
-    end;
+do -- spawncar
+    local spawn_car_function = require(game_folder.Garage.GarageUI.SpawnUI).OnItemSpawnClick._handlerListHead._fn;
 
-    do -- carkick
-        functions.hook_fire_server(car_kick_function, 1, "CarKick");
-
-        pcall(car_kick_function);
-    end;
+    network_keys.SpawnCar = fetch_key(spawn_car_function);
 end;
 
 do -- damage
-    local damage_function = getproto(getproto(dependencies.modules.military_turret_system.init, 1), 1);
-
-    functions.hook_fire_server(damage_function, 1, "Damage");
-
-    pcall(damage_function);
+    local military_added_signal = require(game_folder.MilitaryTurret.MilitaryTurretBinder)._classAddedSignal._handlerListHead._fn;
+    local damage_function = getproto(military_added_signal, 1);
+    
+    network_keys.Damage = fetch_key(damage_function);
 end;
 
 do -- switchteam
-    local switch_team_function = getproto(dependencies.modules.team_choose_ui.Show, 4);
+    local switch_team_function = getproto(team_choose_ui.Show, 4);
 
-    functions.hook_fire_server(switch_team_function, 2, "SwitchTeam");
-
-    pcall(switch_team_function);
+    network_keys.SwitchTeam = fetch_key(switch_team_function);
 end;
 
-do -- broadcastinputbegan / broadcastinputended
-    local equip_function = dependencies.modules.item_system._equip;
-    local input_began_function = getproto(equip_function, 5);
-    local input_ended_function = getproto(equip_function, 6);
-
-    do -- broadcastinputbegan
-        functions.hook_fire_server(input_began_function, 1, "BroadcastInputBegan");
-        pcall(input_began_function, true, {});
-    end;
-
-    do -- broadcastinputended
-        functions.hook_fire_server(input_ended_function, 1, "BroadcastInputEnded");
-        pcall(input_ended_function, true, {});
-    end;
+do -- exitcar
+    local exit_car_function = getupvalue(team_choose_ui.Init, 3);
+    
+    network_keys.ExitCar = fetch_key(exit_car_function);
 end;
 
 do -- taze
-    local taze_function = dependencies.modules.taser.Tase;
-    local old_upvalues = getupvalues(taze_function);
+    local taze_function = require(game_folder.Item.Taser).Tase;
 
-    functions.mark_function(taze_function, "Taze");
-
-    setupvalue(taze_function, 1, {getAttr = function() return 0; end, setAttr = function() end});
-    setupvalue(taze_function, 2, {ObjectLocal = function() end});
-    setupvalue(taze_function, 3, {GetPlayers = function() return {}; end});
-    setupvalue(taze_function, 5, {RayIgnoreNonCollideWithIgnoreList = function() return true; end});
-    setupvalue(taze_function, 6, {getPlayerFromDescendant = function() return {Name = ""}; end});
-
-    pcall(taze_function, {
-        ItemData = {NextUse = 0}, CrossHair = {Flare = function() end, Spring = {Accelerate = function() end}}, 
-        Config = {Sound = {tazer_buzz = 0}, ReloadTime = 0, ReloadTimeHit = 0}, IgnoreList = {}, Draw = function() end, BroadcastInputBegan = function() end, 
-        UpdateMousePosition = function() end, Tip = {Position = vector3_new()}, Local = true, MousePosition = vector3_new()
-    });
-
-    setupvalue(taze_function, 1, old_upvalues[1]); 
-    setupvalue(taze_function, 2, old_upvalues[2]); 
-    setupvalue(taze_function, 3, players); 
-    setupvalue(taze_function, 5, old_upvalues[5]); 
-    setupvalue(taze_function, 6, old_upvalues[6]); 
+    network_keys.Taze = fetch_key(taze_function);
 end;
 
-do -- playsound 
-    for index, connection in next, getconnections(game:GetService("RunService").Heartbeat) do 
-        local connection_function = connection.Function;
-        
-        if type(connection_function) == "function" and table_find(getconstants(connection_function), "Vehicle Heartbeat") then 
-            for index, upvalue in next, getupvalues(connection_function) do 
-                if type(upvalue) == "function" and islclosure(upvalue) and table_find(getconstants(upvalue), "NitroLoop") then 
-                    local play_sound_function = getupvalue(upvalue, 1);
-                    functions.mark_function(play_sound_function, "PlaySound");
+do -- punch
+    local punch_function = getupvalue(default_actions.punchButton.onPressed, 1).attemptPunch;
+    
+    network_keys.Punch = fetch_key(punch_function);
+end;
 
-                    local old_upvalue = getupvalue(play_sound_function, 2);
-                    setupvalue(play_sound_function, 2, setmetatable({}, {__index = function() return function() end; end}));
+do -- falldamage
+    local jump_signal = default_actions.onJumpPressed._handlerListHead._next._fn;
+    local fall_function = getupvalue(getupvalue(getupvalue(jump_signal, 1), 4), 3);
+    
+    network_keys.FallDamage = fetch_key(fall_function);
+end;
 
-                    pcall(play_sound_function, nil, nil);
+do -- pickpocket / arrest
+    local character_added_signal = getconnections(collection_service:GetInstanceAddedSignal("Character"))[1].Function;
+    local interact_function = getupvalue(character_added_signal, 2);
 
-                    setupvalue(play_sound_function, 2, old_upvalue);
-                end;
-            end;
-        end;
-    end;
+    local pickpocket_function = getupvalue(getupvalue(interact_function, 2), 2);
+    local arrest_function = getupvalue(getupvalue(interact_function, 1), 7);
+
+    network_keys.Pickpocket = fetch_key(pickpocket_function);
+    network_keys.Arrest = fetch_key(arrest_function);
+end;
+
+do -- broadcastinputbegan / broadcastinputended
+    local equip_function = require(game_folder.ItemSystem.ItemSystem)._equip;
+
+    local input_began_function = getproto(equip_function, 5);
+    local input_ended_function = getproto(equip_function, 6);
+
+    network_keys.BroadcastInputBegan = fetch_key(input_began_function);
+    network_keys.BroadcastInputEnded = fetch_key(input_ended_function);
 end;
 
 do -- eject / hijack / entercar
-    local connection = getconnections(collection_service:GetInstanceAddedSignal("VehicleSeat"))[1].Function;
-    local seat_interact_function = getupvalue(connection, 1);
+    local seat_added_signal = getconnections(collection_service:GetInstanceAddedSignal("VehicleSeat"))[1].Function;
+    local seat_interact_function = getupvalue(seat_added_signal, 1);
 
     local hijack_function = getupvalue(seat_interact_function, 1);
     local eject_function = getupvalue(seat_interact_function, 2);
     local enter_car_function = getupvalue(seat_interact_function, 3);
 
-    do -- hijack
-        functions.mark_function(hijack_function, "Hijack");
-        pcall(hijack_function);
-    end;
-
-    do -- eject
-        functions.mark_function(eject_function, "Eject");
-        pcall(eject_function);
-    end;
-
-    do -- entercar
-        functions.mark_function(enter_car_function, "EnterCar");
-
-        local old_upvalue = getupvalue(enter_car_function, 1);
-        setupvalue(enter_car_function, 1, nil);
-
-        pcall(enter_car_function, {});
-
-        setupvalue(enter_car_function, 1, old_upvalue);
-    end;
+    network_keys.Hijack = fetch_key(hijack_function);
+    network_keys.Eject = fetch_key(eject_function);
+    network_keys.EnterCar = fetch_key(enter_car_function);
 end;
 
-do -- pickpocket / arrest
-    local connection = getconnections(collection_service:GetInstanceAddedSignal("Character"))[1].Function;
-    local interact_function = getupvalue(connection, 2);
-    local pickpocket_function = getupvalue(getupvalue(interact_function, 2), 2);
-    local arrest_function = getupvalue(getupvalue(interact_function, 1), 7);
-
-    do -- pickpocket
-        functions.mark_function(pickpocket_function, "Pickpocket");
-        pcall(pickpocket_function, {});
-    end;
-
-    do -- arrest
-        functions.mark_function(arrest_function, "Arrest");
-        pcall(arrest_function, {});
-    end;
-end;
-
-do -- falldamage
-    local connection = getconnections(getupvalue(dependencies.modules.falling.Init, 3).Button.MouseButton1Down)[1].Function;
-    local fall_function = getupvalue(getupvalue(getupvalue(connection, 1), 4), 3);
-
-    functions.mark_function(fall_function, "FallDamage");
-
-    local old_upvalues = getupvalues(fall_function);
-
-    setupvalue(fall_function, 1, true);
-    setupvalue(fall_function, 3, function() end);
-    setupvalue(fall_function, 4, function() end);
-    setupvalue(fall_function, 5, 25);
-    setupvalue(fall_function, 6, true);
-
-    pcall(fall_function, 0);
-
-    for index = 1, 6 do 
-        if index ~= 2 then 
-            setupvalue(fall_function, index, old_upvalues[index]);
+do -- playsound
+    for key, client_function in next, getupvalue(team_choose_ui.Init, 2) do 
+        if type(client_function) == "function" and getconstants(client_function)[1] == "Source" then 
+            network_keys.PlaySound = key;
+            
+            break;
         end;
-    end;
-end;
-
-do -- exitcar
-    local exit_car_function = getupvalue(dependencies.modules.team_choose_ui.Init, 3);
-
-    functions.mark_function(exit_car_function, "ExitCar");
-
-    local old_upvalues = getupvalues(exit_car_function);
-
-    setupvalue(exit_car_function, 1, true);
-    setupvalue(exit_car_function, 2, {OnVehicleJumpExited = {Fire = function() end}});
-    setupvalue(exit_car_function, 6, {Heli = {}});
-
-    pcall(exit_car_function);
-
-    setupvalue(exit_car_function, 1, old_upvalues[1]);
-    setupvalue(exit_car_function, 2, old_upvalues[2]);
-    setupvalue(exit_car_function, 6, old_upvalues[6]);
-end;
-
-do -- spawncar (given to me by Tazed#8126)
-    local spawn_car_function = getproto(dependencies.modules.garage_ui.Init, 3);
-
-    functions.hook_fire_server(spawn_car_function, 1, "SpawnCar");
-
-    pcall(spawn_car_function, {});
-end;
-
-do -- redeemcode
-    local redeem_code_function = getproto(dependencies.modules.codes.Init, 4);
-    
-    setupvalue(redeem_code_function, 1, {CodeContainer = {Background = {CodeContainer = {Code = {Text = " "}}}}});
-    setupvalue(redeem_code_function, 3, {WindowClose = function() end})
-    
-    functions.hook_fire_server(redeem_code_function, 2, "RedeemCode");
-    
-    pcall(redeem_code_function);
-end;
-
---// Reset Network
-
-setupvalue(dependencies.network.FireServer, 1, old_fire_server);
-
---// Output Keys 
-
-if shared.output_keys then
-    if rconsolecreate then 
-        rconsolecreate();
     end; 
-    
-    rconsolename("Jailbreak Key Fetcher");
-    rconsolewarn(("Took %s seconds to grab keys!\n"):format(tick() - dependencies.start_time));
-
-    for index, key in next, dependencies.network_keys do 
-        rconsoleprint(("%s : %s\n"):format(index, key));
-    end;
 end;
 
---// Add Keys to Environment 
+--// Return Variables 
 
-if shared.add_to_env then 
-    local environment = getgenv();
+local environment = getgenv();
 
-    environment.keys = dependencies.network_keys;
-    environment.network = dependencies.network;
-end;
+environment.network_keys, environment.network = network_keys, network;
 
---// Check for Missing Keys 
+warn(("Key Fetcher Loaded in %s Seconds"):format(tick() - start_time));
 
-local console_created, console_named = false, false; -- i hate scriptware's console system lol
-
-for index, key_name in next, dependencies.keys_list do 
-    if not dependencies.network_keys[key_name] then 
-    	if rconsolecreate and not console_created and not shared.output_keys then 
-	    console_created = true; 
-	    rconsolecreate();
-	    rconsolename("Jailbreak Key Fetcher");
-	end; 
-    
-        rconsoleerr(("Failed to fetch key %s"):format(key_name));
-
-        if not console_created and not console_named and not shared.output_keys then 
-	    console_named = true;
-            rconsolename("Jailbreak Key Fetcher");
-        end;
-    end;
-end;
-
-return dependencies.network_keys, dependencies.network;
+return network_keys, network;
