03bcb8bc1c1f0f377247b514b960a3315dd555cf
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2022-01-30 09:29:00 -0500

    Create SpeedAndJumpBoost.lua

---
+++ Scripts/Deepwoken/SpeedAndJumpBoost.lua
@@ -0,0 +1,36 @@
+--// variables
+
+local player = game:GetService("Players").LocalPlayer;
+
+local walk_speed, jump_power = shared.walk_speed or 150, shared.jump_power or 250;
+
+--// main hook 
+
+local old_newindex
+old_newindex = hookmetamethod(game, "__newindex", function(self, index, value)
+    if not checkcaller() then
+        if index == "WalkSpeed" then 
+            local character = player.Character;
+
+            if character then 
+                local humanoid = character:FindFirstChildOfClass("Humanoid");
+
+                if humanoid and self == humanoid then
+                    return old_newindex(self, index, walk_speed);
+                end;
+            end;
+        elseif index == "JumpPower" then
+            local character = player.Character; 
+
+            if character then 
+                local humanoid = character:FindFirstChildOfClass("Humanoid");
+
+                if humanoid and self == humanoid then
+                    return old_newindex(self, index, jump_power);
+                end;
+            end;
+        end;
+    end;
+
+    return old_newindex(self, index, value);
+end);
