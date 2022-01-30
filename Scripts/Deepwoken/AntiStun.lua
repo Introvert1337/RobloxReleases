fefb8329a781069ed0181a352e471e2c525d4ca9
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2022-01-30 09:25:42 -0500

    Create AntiStun.lua

---
+++ Scripts/Deepwoken/AntiStun.lua
@@ -0,0 +1,38 @@
+local stun_class_names = {
+    ["PreventAction"] = true,
+    ["OffhandAttack"] = true,
+    ["MobileAction"] = true,
+    ["Unequipping"] = true,
+    ["LightAttack"] = true,
+    ["Unconscious"] = true,
+    ["UsingSpell"] = true,
+    ["Equipping"] = true,
+    ["NoAttack"] = true,
+    ["Carried"] = true,
+    ["Knocked"] = true,
+    ["Action"] = true,
+    ["Pinned"] = true,
+    ["Stun"] = true,
+    ["NoRoll"] = true
+};
+
+local effects_table = require(game:GetService("ReplicatedStorage").EffectReplicator).Effects;
+
+local old_pairs
+old_pairs = replaceclosure(pairs, newcclosure(function(data)
+    if data == effects_table then
+        local fake_effects = {};
+
+        for effect_id, effect_data in next, data do 
+            local effect_class = effect_data.Class; 
+
+            if not stun_class_names[effect_class] then 
+                fake_effects[effect_id] = effect_data;
+            end;
+        end;
+
+        return old_pairs(fake_effects);
+    end;
+
+    return old_pairs(data);
+end));
