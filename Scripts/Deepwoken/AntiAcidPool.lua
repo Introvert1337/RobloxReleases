079246cf4cf35c1a4a52599a86de36ccd9762faf
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2022-01-30 10:11:16 -0500

    Create AntiAcidPool.lua

---
+++ Scripts/Deepwoken/AntiAcidPool.lua
@@ -0,0 +1,18 @@
+--// variables
+
+local acid_check_remote = game:GetService("ReplicatedStorage").Requests.AcidCheck;
+
+--// main hook
+
+local old_namecall 
+old_namecall = hookmetamethod(game, "__namecall", function(self, ...)
+    if self == acid_check_remote then
+        local namecall_method = getnamecallmethod();
+    
+        if namecall_method == "FireServer" or namecall_method == "fireServer" then
+            return;
+        end;
+    end;
+
+    return old_namecall(self, ...);
+end);
