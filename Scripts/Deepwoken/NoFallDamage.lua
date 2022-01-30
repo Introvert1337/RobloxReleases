40b6a817c81a2133c16557f29aad4ba576012199
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2022-01-30 10:09:31 -0500

    Create NoFallDamage.lua

---
+++ Scripts/Deepwoken/NoFallDamage.lua
@@ -0,0 +1,26 @@
+--// variables
+
+local get_remote = loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/main/Scripts/Deepwoken/RemoteBypass.lua"))();
+
+local fall_damage_remote = get_remote("FallDamage");
+
+--// re-grab remote on respawn
+
+game:GetService("Players").LocalPlayer.CharacterAdded:Connect(function(character)
+    fall_damage_remote = character:WaitForChild("CharacterHandler"):WaitForChild("Requests").FallDamage;
+end);
+
+--// main hook
+
+local old_namecall 
+old_namecall = hookmetamethod(game, "__namecall", function(self, ...)
+    local namecall_method = getnamecallmethod();
+
+    if namecall_method == "FireServer" or namecall_method == "fireServer" then
+        if self == fall_damage_remote then
+            return;
+        end;
+    end;
+
+    return old_namecall(self, ...);
+end);
