16570b97d769750cab5d2b66614821547ad179bf
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2022-01-30 10:15:46 -0500

    Update AutoCharisma.lua

--- Scripts/Deepwoken/AutoCharisma.lua
+++ Scripts/Deepwoken/AutoCharisma.lua
@@ -10,7 +10,7 @@ game:GetService("Players").LocalPlayer.PlayerGui.ChildAdded:Connect(function(chi
         local correct_text = child.Prompt.Text:match("Try some small talk on someone nearby%.\n'(.+)'");
         
         if correct_text then 
-            wait(math.random(3, 5)); -- delay to make it more legit
+            task.wait(math.random(3, 5)); -- delay to make it more legit
 
             players:Chat(correct_text);
             send_chat:FireServer(correct_text, "All");
