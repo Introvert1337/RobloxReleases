c9986ea7287b7461dff01c7a49d57ee450156f6b
Spencer Cohen <66886179+Introvert1337@users.noreply.github.com>
2022-03-21 21:38:28 -0400

    Update HttpSpy.lua

--- Scripts/Utilities/HttpSpy.lua
+++ Scripts/Utilities/HttpSpy.lua
@@ -91,7 +91,7 @@ do
             return old_syn_request(payload);
         end;
       
-      if payload_clone.Body and (not payload_clone.Method or payload_clone.Method == "GET" or payload_clone.Method == "HEAD") then 
+      if type(payload_clone.Body) == "string" and (not payload_clone.Method or payload_clone.Method == "GET" or payload_clone.Method == "HEAD") then 
           return old_syn_request(payload);
       end;
         
