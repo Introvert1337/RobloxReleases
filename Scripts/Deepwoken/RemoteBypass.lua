ae23846232e640fd50d6353569eaa8727319d992
Spencer <66886179+Introvert1337@users.noreply.github.com>
2022-04-03 18:00:16 -0400

    Update RemoteBypass.lua

--- Scripts/Deepwoken/RemoteBypass.lua
+++ Scripts/Deepwoken/RemoteBypass.lua
@@ -1,11 +1,34 @@
-local ui_manager = game:GetService("Players").LocalPlayer.PlayerScripts.UIManager;
-local text_service_connection = getconnections(game:GetService("TextService").ChildAdded)[1].Function;
+local virtual_input_manager = game:GetService("VirtualInputManager");
 
-local ui_manager_get_remote = getupvalue(text_service_connection, 1);
-
-local get_key_verification_key = getconstant(ui_manager_get_remote, 1); -- plum
-local get_key_function = getupvalue(ui_manager_get_remote, 1)[1];
+local roll_function = getsenv(game:GetService("Players").LocalPlayer.Character.CharacterHandler.InputClient).Roll;
+local remote_constant_index = table.find(getconstants(roll_function), "Unblock");
 
 return function(remote_name)
-    return syn.secure_call(get_key_function, ui_manager, remote_name, get_key_verification_key); -- synapse only
+    local old_upvalues = getupvalues(roll_function);
+    
+    setupvalue(roll_function, 1, {
+        FindEffect = function(self, effect_name)
+            return effect_name == "Blocking";
+        end;
+    });
+    
+    local thread = coroutine.running();
+    
+    setupvalue(roll_function, 3, newcclosure(function(remote)
+        setupvalue(roll_function, 1, old_upvalues[1]);
+        setupvalue(roll_function, 3, old_upvalues[3]);
+        
+        setconstant(roll_function, remote_constant_index, "Unblock");
+        
+        coroutine.resume(thread, remote);
+        
+        coroutine.yield();
+    end));
+    
+    setconstant(roll_function, remote_constant_index, remote_name);
+    
+    virtual_input_manager:SendKeyEvent(true, "Q", false, game);
+    virtual_input_manager:SendKeyEvent(false, "Q", false, game);
+    
+    return coroutine.yield();
 end;
