4f3450959448119d88ad196ef48f32d4447e8321
Spencer <66886179+Introvert1337@users.noreply.github.com>
2023-01-26 12:59:14 -0500

    somehow this still works so changed from shitty coding style

--- Scripts/Deepwoken/RemoteBypass.lua
+++ Scripts/Deepwoken/RemoteBypass.lua
@@ -1,34 +1,34 @@
-local virtual_input_manager = game:GetService("VirtualInputManager");
+local VirtualInputManager = game:GetService("VirtualInputManager")
 
-local roll_function = getsenv(game:GetService("Players").LocalPlayer.Character.CharacterHandler.InputClient).Roll;
-local remote_constant_index = table.find(getconstants(roll_function), "Unblock");
+local rollFunction = getsenv(game:GetService("Players").LocalPlayer.Character.CharacterHandler.InputClient).Roll
+local remoteConstantIndex = table.find(getconstants(rollFunction), "Unblock")
 
-return function(remote_name)
-    local old_upvalues = getupvalues(roll_function);
+return function(remoteName)
+    local oldUpvalues = getupvalues(rollFunction)
     
-    setupvalue(roll_function, 1, {
-        FindEffect = function(self, effect_name)
-            return effect_name == "Blocking";
-        end;
-    });
+    setupvalue(rollFunction, 1, {
+        FindEffect = function(self, effectName)
+            return effectName == "Blocking"
+        end
+    })
     
-    local thread = coroutine.running();
+    local thread = coroutine.running()
     
-    setupvalue(roll_function, 3, newcclosure(function(remote)
-        setupvalue(roll_function, 1, old_upvalues[1]);
-        setupvalue(roll_function, 3, old_upvalues[3]);
+    setupvalue(rollFunction, 3, newcclosure(function(remote)
+        setupvalue(rollFunction, 1, oldUpvalues[1])
+        setupvalue(rollFunction, 3, oldUpvalues[3])
         
-        setconstant(roll_function, remote_constant_index, "Unblock");
+        setconstant(rollFunction, remoteConstantIndex, "Unblock")
         
-        coroutine.resume(thread, remote);
+        coroutine.resume(thread, remote)
         
-        coroutine.yield();
-    end));
+        coroutine.yield()
+    end))
     
-    setconstant(roll_function, remote_constant_index, remote_name);
+    setconstant(rollFunction, remoteConstantIndex, remoteName)
     
-    virtual_input_manager:SendKeyEvent(true, "Q", false, game);
-    virtual_input_manager:SendKeyEvent(false, "Q", false, game);
+    VirtualInputManager:SendKeyEvent(true, "Q", false, game)
+    VirtualInputManager:SendKeyEvent(false, "Q", false, game)
     
-    return coroutine.yield();
-end;
+    return coroutine.yield()
+end
