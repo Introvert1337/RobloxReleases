22fb4f8356bebe071e03fefffa8bc3c72b6e5e98
Spencer <66886179+Introvert1337@users.noreply.github.com>
2023-05-04 06:05:48 -0400

    optimized and cleaned up

--- Scripts/RoBeats/AccuracyHelper.lua
+++ Scripts/RoBeats/AccuracyHelper.lua
@@ -1,46 +1,38 @@
 -- this will make great hits perfect and okay hits great
 
-local function validateNoteResultFunction(moduleFunction)
-    local constants, upvalues = getconstants(moduleFunction), getupvalues(moduleFunction)
-    
-    if #constants == 4 and #upvalues == 1 then
-        local possibleNoteResults = upvalues[1]
-        
-        if type(possibleNoteResults) == "table" and rawget(possibleNoteResults, constants[2]) then
-            return possibleNoteResults, constants
-        end
-    end
-end
-
 for index, module in next, getloadedmodules() do
-    if not module.Parent then
-        local moduleValue = require(module)
+	local moduleValue = require(module)
 
-        if type(moduleValue) == "table" and rawget(moduleValue, "lookat_matrix") then
-            for functionName, functionValue in next, moduleValue do
-                if type(functionValue) == "function" then
-                    local noteResults, functionConstants = validateNoteResultFunction(functionValue)
-                    
-                    if noteResults then
-                        local perfectNoteResult = noteResults[functionConstants[4]]
-                        local greatNoteResult = noteResults[functionConstants[3]]
-                        local okayNoteResult = noteResults[functionConstants[2]]
-                        
-                        local oldGetNoteResult = moduleValue[functionName]
-                        moduleValue[functionName] = function(...)
-                            local realNoteResult = oldGetNoteResult(...)
-                            
-                            if realNoteResult == okayNoteResult then
-                                return greatNoteResult
-                            elseif realNoteResult == greatNoteResult then
-                                return perfectNoteResult
-                            end
-                            
-                            return realNoteResult
-                        end
-                    end
-                end
-            end
-        end
+	if typeof(moduleValue) == "table" and rawget(moduleValue, "lookat_matrix") then
+		for functionName, functionValue in next, moduleValue do
+			if typeof(functionValue) == "function" then
+				local constants, upvalues = getconstants(functionValue), getupvalues(functionValue)
+
+				if #constants == 4 and #upvalues == 1 then
+					local noteResults = upvalues[1]
+					
+					if typeof(noteResults) == "table" and rawget(noteResults, constants[2]) then
+						local perfectNoteResult = noteResults[constants[4]]
+						local greatNoteResult = noteResults[constants[3]]
+						local okayNoteResult = noteResults[constants[2]]
+						
+						local oldGetNoteResult = moduleValue[functionName]
+						moduleValue[functionName] = function(...)
+							local realNoteResult = oldGetNoteResult(...)
+							
+							if realNoteResult == okayNoteResult then
+								return greatNoteResult
+							elseif realNoteResult == greatNoteResult then
+								return perfectNoteResult
+							end
+							
+							return realNoteResult
+						end
+						
+						return warn("Successfully hooked note results!")
+					end
+				end
+			end
+		end
     end
 end
